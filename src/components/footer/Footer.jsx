25449ac2136ffddee02d681ad3b95bed99fb9916
Mathias Boll <math50v7@mcdmstudent.dk>
2025-11-12 12:39:08 +0100

    footer fiks

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,10 +4,27 @@ import logo from "/logo.png";
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <div className={styles.soMe}></div>
-      <div>
-        <img src={logo} alt='logo' />
-        <h5>Gittes Glamping</h5>
+      <div className={styles.footerInner}>
+
+        {/* Sociale medier */}
+        <div className={styles.footerSocial}>
+          <a className={styles.footerIcon} href="/">
+            <img src="src/assets/icons/square-facebook.svg" alt="Facebook" />
+          </a>
+
+          <a className={styles.footerIcon} href="/">
+            <img src="src/assets/icons/square-instagram.svg" alt="Instagram" />
+          </a>
+        </div>
+
+        {/* Brand */}
+        <div className={styles.footerBrand}>
+          <a href="/">
+            <img src={logo} alt="Gittes Glamping logo" className={styles.footerLogo} />
+          </a>
+          <span className={styles.footerText}>Gittes Glamping</span>
+        </div>
+
       </div>
     </footer>
   );
