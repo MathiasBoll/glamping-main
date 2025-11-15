47e399828fe51f47b70a6a11ccca285ca41eb62e
Mathias Boll <math50v7@mcdmstudent.dk>
2025-11-15 18:22:05 +0100

    opdatering af stay siden

--- src/components/stay/Stay.jsx
+++ src/components/stay/Stay.jsx
@@ -7,20 +7,20 @@ const Stay = ({ stay }) => {
 
   return (
     <figure className={styles.stay}>
-      {/* Toppens beige titelboks */}
+      {/* TOP: beige titelboks */}
       <figcaption className={styles.caption}>
         <p className={styles.title}>{stay.title}</p>
         <p className={styles.titleTwo}>{stay.numberOfPersons} personer</p>
         <p className={styles.titleTwo}>Fra {stay.price}</p>
       </figcaption>
 
-      {/* Kvadratisk billede */}
+      {/* MIDT: billede */}
       <img className={styles.image} src={stay.image} alt={stay.title} />
 
-      {/* Knap nederst */}
-      <div className={styles.buttonWrap}>
+      {/* BUND: teal bar med beige knap */}
+      <div className={styles.bottomBar}>
         <Button
-          buttonText="Læs mere"
+          buttonText='Læs mere'
           onClick={() => navigate(`/stay/${stay._id}`)}
         />
       </div>
